d6dea65a0e6053ef26665d6170d9fcc27a4af52b
redsled84 <hmnotsocool@gmail.com>
2016-04-10 01:56:10 -0700

    init commit

---
+++ main.lua
@@ -0,0 +1,12 @@
+function love.load()
+	local str = "Astrosmash copy currently under development by redsled84 and slainchicken :)"
+	print(str)
+end
+
+function love.update(dt)
+	
+end
+
+function love.draw()
+
+end
