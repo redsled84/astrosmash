f7068f342038749a5067f2cbda74b60bf68b4c8b
garry <garry.stanfill@gmail.com>
2016-04-11 17:48:05 -0500

    Adding a background

---
+++ background.lua
@@ -0,0 +1,16 @@
+local Background = {}
+
+function Background:createBackground()
+	self.x = love.math.random(10, width)
+	self.y = love.math.random(10, height)
+	for i=1,10 do
+		table.insert(self, { self.x, self.y })
+	end
+end
+
+function Background:drawBackground()
+	for i,v in ipairs(Background) do
+		love.graphics.rectangle(fill, x, y, 3, 3)
+	end
+end
+
