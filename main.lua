987ed1e2498a70fd6b66681f0fd801aad1677638
garry <garry.stanfill@gmail.com>
2016-04-10 21:33:49 -0500

    enemies test

--- main.lua
+++ main.lua
@@ -1,12 +1,20 @@
+require ("enemies") 
+listOfEnemies = {}
+
 function love.load()
-	local str = "Astrosmash copy currently under development by redsled84 and slainchicken :)"
+	local str = "Astrosmash copy currently under development by redsled84 and slainchicken and LapisButterfly :)"
 	print(str)
 end
 
 function love.update(dt)
-	
+	for i=1,100 do
+		table.insert(listOfEnemies, createEnemy())
+	end	
+	moveEnemy()
 end
 
 function love.draw()
-
+	for i,v in ipairs(listOfEnemies) do
+		drawEnemy(5,5,5,5)
+	end
 end
