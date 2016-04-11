df4a3c1c0b11ce8463d2fc68af0ad550eaeed9bc
garry <garry.stanfill@gmail.com>
2016-04-11 17:19:04 -0500

    Updated enemies to create 10 and move

--- enemies.lua
+++ enemies.lua
@@ -1,24 +1,59 @@
-local Enemies = {}
+local Enemies = { w = 16, h = 32 }
 
---listOfEnemies = {}
-
-function Enemies:createEnemy(x, y, width, height)
-	enemy = {}
-	enemy.x  = x
-	enemy.y = y
-	enemy.width =  10
-	enemy.height = 10
-	return enemy
+function Enemies:spawnEnemy(x, y, spd, w, h)
+	table.insert(self, {x = x, y = y, spd = spd, w = w, h = h, hasCollided = false})
 end
 
-function Enemies:drawEnemy(x, y, width, height)
-	for i,v in ipairs(Enemies) do
-		love.graphics.rectangle("fill", v.x, v.y, v.width, v.height)
+function Enemies:destroyEnemyOnCollide()
+	for i=#self, 1, -1 do
+		if self.hasCollided then
+			table.remove(self, i)
+		end
 	end
 end
 
-function Enemies:moveEnemy()
+function Enemies:destroyEnemyWhenOutOfBounds()
+	for i=#self, 1, -1 do
+		local enemy = self[i]
+		if enemy.y+enemy.h < 0 then
+			table.remove(self, i)
+		end
+	end
+end
 
+function Enemies:moveEnemies(dt)
+	for i=1, #self do
+		local enemy = self[i]
+		enemy.y = enemy.y + enemy.spd * dt
+	end
+end
+
+function Enemies:updateEnemies(dt)
+	x = love.math.random(0, width)
+	spd = love.math.random(50, 200)
+	w = love.math.random(8, 32)
+	h = love.math.random(8, 32)
+    if #Enemies <= 10 then	
+	  self:spawnEnemy(x,0,spd,w,h)
+	end
+	self:moveEnemies(dt)
+	self:destroyEnemyOnCollide()
+	self:destroyEnemyWhenOutOfBounds()
+end
+
+function Enemies:drawEnemies(colors, drawType)
+	for i=1, #self do
+		local enemy = self[i]
+		love.graphics.setColor(unpack(colors))
+		love.graphics.rectangle(drawType, enemy.x, enemy.y, enemy.w, enemy.h)
+	end
+end
+
+function Enemies:shootEnemy(key, x, y)
+	if key == 'space' then
+		self:spawnEnemy(x, y, 500)
+	end
 end
 
 return Enemies
+
