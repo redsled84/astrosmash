0415472864d5e89ccd3931292d69c4b153227dad
garry <garry.stanfill@gmail.com>
2016-04-11 20:03:03 -0500

    Getting some X movement for the enemies

--- enemies.lua
+++ enemies.lua
@@ -2,8 +2,8 @@ local Enemies = { w = 16, h = 32 }
 local windowWidth = love.graphics.getWidth()
 local windowHeight = love.graphics.getHeight()
 
-function Enemies:spawnEnemy(x, y, spd, w, h)
-	table.insert(self, {x = x, y = y, spd = spd, w = w, h = h, hasCollided = false})
+function Enemies:spawnEnemy(x, y, spdX, spdY, w, h)
+	table.insert(self, {x = x, y = y, spdX = spdX, spdY = spdY, w = w, h = h, hasCollided = false})
 end
 
 function Enemies:destroyEnemyOnCollide()
@@ -39,17 +39,19 @@ end
 function Enemies:moveEnemies(dt)
 	for i=1, #self do
 		local enemy = self[i]
-		enemy.y = enemy.y + enemy.spd * dt
+		enemy.x = enemy.x + enemy.spdX * dt
+		enemy.y = enemy.y + enemy.spdY * dt
 	end
 end
 
 function Enemies:updateEnemies(dt)
 	x = love.math.random(0, windowWidth)
-	spd = love.math.random(50, 200)
-	w = love.math.random(8, 32)
+	spdX = love.math.random(-50, 50)
+	spdY = love.math.random(60, 100)
+	w = love.math.random(16, 32)
 	h = love.math.random(8, 32)
     if #Enemies < 2 then	
-		self:spawnEnemy(x, 0, spd, w, h)
+		self:spawnEnemy(x, 0, spdX, spdY, w, h)
 	end
 	self:moveEnemies(dt)
 	self:destroyEnemyOnCollide()
@@ -66,7 +68,7 @@ end
 
 function Enemies:shootEnemy(key, x, y)
 	if key == 'space' then
-		self:spawnEnemy(x, y, 500)
+		self:spawnEnemy(x, y, 500, 200)
 	end
 end
 
