b2b2341bda81389e4a2bb5a2c4d6613dbd078e58
garry <garry.stanfill@gmail.com>
2016-04-20 13:20:42 -0500

    added states for splash screen

--- main.lua
+++ main.lua
@@ -1,33 +1,43 @@
+gameState = "splash"
 score = 0
 lives = 3
+local SplashBackground = require 'splashBackground'
 local Background = require 'background'
 local Player = require 'player'
 local Bullets = require 'bullets'
 local Enemies = require 'enemies'
 
 function love.load()
-	Background:createBackground()
-	Player:setPlayer()
-	local str = "Astrosmash copy currently under development by redsled84, slainchicken and LapisButterfly :)"
-	print(str)
+	if gameState == "splash" then
+	else
+		Background:createBackground()
+		Player:setPlayer()
+		local str = "Astrosmash copy currently under development by redsled84, slainchicken and LapisButterfly :)"
+		print(str)
+	end
 end
 
 function love.update(dt)
-	Player:updatePlayer(dt)
-	Bullets:updateBullets(dt)
-	Enemies:updateEnemies(dt)
-	Bullets:loopBullets(function(bullet)
-		Enemies:checkOnCollide(bullet)
-	end)
+	if gameState == "splash" then
+	else
+		Player:updatePlayer(dt)
+		Bullets:updateBullets(dt)
+		Enemies:updateEnemies(dt)
+		Bullets:loopBullets(function(bullet) Enemies:checkOnCollide(bullet) end)
+	end
 end
 
 function love.draw()
-	Background:drawBackground({0, 255, 0}, 'fill')
-	Player:drawPlayer({0, 0, 255}, 'fill')
-	Bullets:drawBullets({0,0,255}, 'fill')
-	Enemies:drawEnemies({255,0,0}, 'fill')
-	Background:drawScore({0, 255, 0}, 'fill')
-	Background:drawLives({0, 255, 0}, 'fill')
+	if gameState == "splash" then
+		SplashBackground:drawSplashBackground({0, 255, 0}, 'fill')
+	else
+		Background:drawBackground({0, 255, 0}, 'fill')
+		Player:drawPlayer({0, 0, 255}, 'fill')
+		Bullets:drawBullets({0,0,255}, 'fill')
+		Enemies:drawEnemies({255,0,0}, 'fill')
+		Background:drawScore({0, 255, 0}, 'fill')
+		Background:drawLives({0, 255, 0}, 'fill')
+	end
 end
 
 function love.keypressed(key)
